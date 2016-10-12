56a336b19f79a1bd396a744c0e9572b715ec385d
Wiktor <wiktor.ja@interia.pl>
2016-10-12 21:32:19 +0200

    wykonczenie_kalkulatorow

--- Aplikacja/Aplikacja/KalkSpalanie.xaml.cs
+++ Aplikacja/Aplikacja/KalkSpalanie.xaml.cs
@@ -22,6 +22,80 @@ namespace Aplikacja
         public KalkSpalanie()
         {
             InitializeComponent();
+            Bindowanie();
         }
+        private string[] aktywnosci = { "akt1", "akt2", "akt3", "akt4" };
+        private double[] wspolczynniki = { 1, 2, 3, 4 };
+
+        private void Bindowanie()
+        {
+            opisLabel.Content = "opis lalalalalaalalalaal";
+            aktywnosciCombo.ItemsSource = aktywnosci.ToList();
+            wynikLabel.Content = "Uzupełnij pola i kliknij oblicz aby sprawdzić spalanie";
+        }
+
+
+        private void wagaTextbox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.OemComma || e.Key == Key.Delete || e.Key == Key.D0 || e.Key == Key.Back || e.Key == Key.D1 || e.Key == Key.D2 || e.Key == Key.D3 || e.Key == Key.D4 || e.Key == Key.D5 || e.Key == Key.D6 || e.Key == Key.D7 || e.Key == Key.D8 || e.Key == Key.D9 || e.Key == Key.NumPad1 || e.Key == Key.NumPad0 || e.Key == Key.NumPad2 || e.Key == Key.NumPad3 || e.Key == Key.NumPad4 || e.Key == Key.NumPad5 || e.Key == Key.NumPad6 || e.Key == Key.NumPad7 || e.Key == Key.NumPad8 || e.Key == Key.NumPad9)
+                e.Handled = false;
+            else
+                e.Handled = true;
+        }
+
+        private void czasTextbox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.OemComma || e.Key == Key.Delete || e.Key == Key.D0 || e.Key == Key.Back || e.Key == Key.D1 || e.Key == Key.D2 || e.Key == Key.D3 || e.Key == Key.D4 || e.Key == Key.D5 || e.Key == Key.D6 || e.Key == Key.D7 || e.Key == Key.D8 || e.Key == Key.D9 || e.Key == Key.NumPad1 || e.Key == Key.NumPad0 || e.Key == Key.NumPad2 || e.Key == Key.NumPad3 || e.Key == Key.NumPad4 || e.Key == Key.NumPad5 || e.Key == Key.NumPad6 || e.Key == Key.NumPad7 || e.Key == Key.NumPad8 || e.Key == Key.NumPad9)
+                e.Handled = false;
+            else
+                e.Handled = true;
+        }
+
+        private void obliczButton_Click(object sender, RoutedEventArgs e)
+        {
+            string walidacja = "";
+            double waga=0;
+            int indeks=aktywnosciCombo.SelectedIndex;
+            double czas=0;
+            double aktywnosc = 0;
+            try
+            {
+                waga = double.Parse(wagaTextbox.Text.Trim());
+            }
+            catch (Exception)
+            {
+                walidacja = walidacja + " \nWpisałeś błędną wartość w pole waga";
+            }
+            try
+            {
+                czas = double.Parse(czasTextbox.Text.Trim());
+            }
+            catch (Exception)
+            {
+                walidacja = walidacja + " \nWpisałeś błędną wartość w pole czas";
+            }
+            if(aktywnosciCombo.SelectedIndex==-1)
+            {
+                walidacja = walidacja + " \nNie wybrałeś żadnej aktywności";
+            }
+            if (walidacja == "")
+            {
+                aktywnosc = wspolczynniki[indeks]; 
+                wynikLabel.Content = "Twoja spalanie: "+Spalanie(aktywnosc,waga,czas);
+            }
+            else
+            {
+                walidacja = "Wystąpiły błędy przy wpisywaniu danych:" + walidacja;
+                MessageBox.Show(walidacja, "Uwaga", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+        }
+
+        private double Spalanie(double aktywnosc, double waga, double czas)
+        {
+            double wynik = 0;
+            wynik = aktywnosc * waga * czas;
+            return wynik;
+        }
+
     }
 }
