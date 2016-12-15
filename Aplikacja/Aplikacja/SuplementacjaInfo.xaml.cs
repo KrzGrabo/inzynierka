6ede47aa5fa52d62b870d9678f490132243c6200
Wiktor <wiktor.ja@interia.pl>
2016-12-15 17:14:44 +0100

    suplementacja

---
+++ Aplikacja/Aplikacja/SuplementacjaInfo.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Aplikacja
+{
+    /// <summary>
+    /// Interaction logic for SuplementacjaInfo.xaml
+    /// </summary>
+    public partial class SuplementacjaInfo : Window
+    {
+        public SuplementacjaInfo()
+        {
+            InitializeComponent();
+        }
+    }
+}
