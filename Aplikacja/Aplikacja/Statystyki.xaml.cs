ec6e8a41e7d5710b53789a9b7b218b93c4cd0d61
Wiktor <wiktor.ja@interia.pl>
2017-01-06 18:34:28 +0100

    poprawki

--- Aplikacja/Aplikacja/Statystyki.xaml.cs
+++ Aplikacja/Aplikacja/Statystyki.xaml.cs
@@ -26,20 +26,52 @@ namespace Aplikacja
         }
         public void Bindowanie()
         {
-            List<Danee> dana = new List<Danee>();
-            dana.Add(new Danee() { nazwa = "nazwa", numerek = 2 });
-            dana.Add(new Danee() { nazwa = "n21azwa", numerek = 3 });
-            dana.Add(new Danee() { nazwa = "na21232zwa", numerek = 5 });
-            dana.Add(new Danee() { nazwa = "naz3232wa", numerek = 1 });
-            columnChart.DataContext = dana;
-            pieChart.DataContext = dana;
+            List<obwodPasaStat> dana = new List<obwodPasaStat>();
+            dana.Add(new obwodPasaStat() { data = "nazwa", wartosc = 2 });
+            dana.Add(new obwodPasaStat() { data = "n21azwa", wartosc = 3 });
+            dana.Add(new obwodPasaStat() { data = "na21232zwa", wartosc = 5 });
+            dana.Add(new obwodPasaStat() { data = "naz3232wa", wartosc = 1 });
+            
+            
+            
+            dietaChart.DataContext = dana;
+            pasChart.DataContext = dana;
+            wagaChart.DataContext = dana;
+            biodraChart.DataContext = dana;
+            treningChart.DataContext = dana;
         }
 
+
+        ////NAJPIERW TRZEBA DATY(chyba bo nie wiem jak on to posortuje) DO STRINGOW PRZEKONWERTOWAC DOPIERO POTEM WRZUCAC DO STRINGOW W KLASACH, CHOCIAŻ NIE WIEM 
+
     }
-    public class Danee
+    public class obwodPasaStat
+    {
+        public string data { get; set; }
+        public double wartosc { get; set; }
+    }
+
+    public class obwodBioderStat
+    {
+        public string data { get; set; }
+        public double wartosc { get; set; }
+    }
+
+    public class wagaStat
+    {
+        public string data { get; set; }
+        public double wartosc { get; set; }
+    }
+
+    public class treningiStat
+    {
+        public string typ { get; set; }
+        public double czas { get; set; }
+    }
+
+    public class dietaStat
     {
         public string nazwa { get; set; }
         public int numerek { get; set; }
     }
-
 }
